05525b4ef9f349c77cd73f68819901bcb7ea4207
Ahmed <ghoul.ahmed5@gmail.com>
2017-12-24 22:41:13 +0100

    add uglify webpack module

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,8 @@
 import  {log}   from './log.js'
+// import  $   from 'jquery'
 let a  =  "salut"
 log(a)
+
+console.log('test');
+
+// $('body').append('salut')
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
-const path = require('path')
-module.exports = {
+const path = require("path");
+const webpack = require("webpack");
+const uglifyjs = require("uglifyjs-webpack-plugin");
+const dev = process.env.NODE_ENV === "dev";
 
-  entry:'./assets/app.js',
-  watch: true,
-  output:{
-    path:path.resolve('./dist'),
-    filename:'bundle.js'
+let config = {
+  entry: "./assets/app.js",
+  watch: dev,
+  output: {
+    path: path.resolve("./dist"),
+    filename: "bundle.js"
   },
-
-  module:{
-    rules:[
+  devtool: "source-map",
+  module: {
+    rules: [
       {
-        test:/\.js$/,
-        exclude:/(node_modules|bower_components)/,
-        use:['babel-loader']
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ["babel-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin({
+    //   minimize: true
+    // })
+  ]
+};
 
-}
+if (!dev) config.plugins.push(new uglifyjs({sourceMap : true}));
+
+module.exports = config;
